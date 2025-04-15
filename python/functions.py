07d45b8d66bff511322c0dca6a51391020553cb2
igurupreeth <igurupreeth@gmail.com>
2025-04-15 13:16:13 +0530

    functions in python

--- python/functions.py
+++ python/functions.py
@@ -1,9 +1,145 @@
-# declaring a function in python using def keywork. 
+# # declaring a function in python using def keywork. 
 
-def add():
-      a = 10 
-      b = 20
-      print(a+b)
+# def add():
+#       a = 10 
+#       b = 20
+#       print(a+b)
 
-# calling the function by its name. 
-add()
+# # calling the function by its name. 
+# add()
+
+# # 1. functions without any parameters. 
+# def subract():
+#       a = 10
+#       b = 5
+#       print(a-b)
+
+# subract()
+
+# # functions with paramers. 
+# def multiply(a,b):
+#       print(a*b)
+
+# multiply(2,5)
+
+# # function with return valuse. 
+# def greeting(name):
+#       return "Hello {}".format(name)
+
+
+# print(greeting("aditya"))
+# print(greeting("ecoders"))
+
+# # functions with default values. 
+# def greeting(name="Ecoders"):
+#       return "Hello {}".format(name)
+
+# print(greeting("aditya"))
+# print(greeting("ecoders"))
+# print(greeting())
+
+# inbuilt functions. 
+
+# print(dir())
+
+# print(dir(__builtins__))
+
+# s1 = "aditya"
+# print(len(s1))
+
+# l1 = [1,2,3,4,5,6,7,8,9,10]
+# print(min(l1))
+# print(max(l1))
+
+# advance functions in python. 
+# map(), filter(), zip(), reduce()
+
+l1 = [1,2,3,4,5,6,7,8,9,10]
+
+def square(num):
+      return num* num
+
+print(list(map(square, l1)))
+
+
+# l1.map((eachnumer)=>{
+#       return eachnumber * eachnumber
+# })
+
+
+
+# l1 = [1,2,3,4,5,6,7,8,9,10]
+
+# def even(num):
+#       if num % 2 == 0:
+#             return num
+
+
+# def odd(num):
+#       if num % 2 != 0:
+#             return num
+
+# print(list(filter(even, l1)))
+
+# print(list(filter(odd, l1)))
+
+
+# un = ["adiya" , "vankatesh" , "Shilpa"]
+# pwd = ["1234" , "", "4312"]
+
+# print(list(zip(un, pwd)))
+
+
+# # reduce function. 
+# from functools import reduce
+
+# def add(a, b):
+#       return a+b
+
+
+# l1 = [1,2,3,4,5,6,7,8,9,10]
+
+# # 1,2,3,4,5,6,7,8,9,10
+# # 3,3,4,5,6,7,8,9,10
+# # 6,4,5,6,7,8,9,10
+# # 10,5,6,7,8,9,10
+# # 15,6,7,8,9,10
+# # 21,7,8,9,10
+# # 28,8,9,10
+# # 36,9,10
+# # 45,10
+# # 55
+
+# print(reduce(add, l1, 0))
+
+# def multiply(a,b):
+#       return a*b
+
+# l1 = [1,2,3,4,5]
+
+# print(reduce(multiply, l1, 1))
+
+
+
+# recursive functions. 
+
+def sum(num):
+      if num == 1:
+            return 1
+      else:
+            return num + sum(num-1)
+
+# print(sum(1))
+
+# print(sum(2))
+
+print(dir(list))
+
+# l1 = [1,2,3,4,5,6,7,8,9,10]
+
+# for i in l1:
+#       print(sum(i))
+#       print("-" * 40)
+
+
+print(sum(100))
--- python/make_pythonnotes.py
+++ python/make_pythonnotes.py
@@ -3,54 +3,153 @@ myfile = open("python_notes.txt" , "a")
 my_notes = """
 
 ------------------------------------------------------------------------------
-topic 3: # if, if-else,  if-elif-else, break, continue, 
+topic 4 : Functions in python. 
 -------------------------------------------------------------------------------
+# # declaring a function in python using def keywork. 
 
-name = "raju"
+# def add():
+#       a = 10 
+#       b = 20
+#       print(a+b)
 
-if name == "raj":
-      print("access granted.")
-elif name == "raju":
-      print("access granted.")
-elif name == "rajesh":
-      print("access granted.")
-elif name == "ramesh":
-      print("access granted.")
-else:
-      print("Access denied")
+# # calling the function by its name. 
+# add()
+
+# # 1. functions without any parameters. 
+# def subract():
+#       a = 10
+#       b = 5
+#       print(a-b)
+
+# subract()
+
+# # functions with paramers. 
+# def multiply(a,b):
+#       print(a*b)
+
+# multiply(2,5)
+
+# # function with return valuse. 
+# def greeting(name):
+#       return "Hello {}".format(name)
 
 
-# break , 
-for i in range(1,11):
-      print(i, end=" ")
+# print(greeting("aditya"))
+# print(greeting("ecoders"))
 
-print()
-print("*" * 50)
+# # functions with default values. 
+# def greeting(name="Ecoders"):
+#       return "Hello {}".format(name)
 
-for i in range(1,11):
-      print(i, end=" ")
-      if(i == 5):
-            break
+# print(greeting("aditya"))
+# print(greeting("ecoders"))
+# print(greeting())
 
-print()
-print("*" * 60)
+# inbuilt functions. 
 
-for i in range(1, 11):
-      if i== 5:
-            continue
-      print(i, end = " ")
+# print(dir())
 
-print()
-print("*" * 60)
+# print(dir(__builtins__))
 
- logical "and" operators
- logical "or"  operators
- logical "not" operators
+# s1 = "aditya"
+# print(len(s1))
 
-for i in range(1, 11):
-      if i >= 4 and i<= 7:
-            continue
-      print(i, end = " ")
+# l1 = [1,2,3,4,5,6,7,8,9,10]
+# print(min(l1))
+# print(max(l1))
+
+# advance functions in python. 
+# map(), filter(), zip(), reduce()
+
+l1 = [1,2,3,4,5,6,7,8,9,10]
+
+def square(num):
+      return num* num
+
+print(list(map(square, l1)))
+
+
+# l1.map((eachnumer)=>{
+#       return eachnumber * eachnumber
+# })
+
+
+
+# l1 = [1,2,3,4,5,6,7,8,9,10]
+
+# def even(num):
+#       if num % 2 == 0:
+#             return num
+
+
+# def odd(num):
+#       if num % 2 != 0:
+#             return num
+
+# print(list(filter(even, l1)))
+
+# print(list(filter(odd, l1)))
+
+
+# un = ["adiya" , "vankatesh" , "Shilpa"]
+# pwd = ["1234" , "", "4312"]
+
+# print(list(zip(un, pwd)))
+
+
+# # reduce function. 
+# from functools import reduce
+
+# def add(a, b):
+#       return a+b
+
+
+# l1 = [1,2,3,4,5,6,7,8,9,10]
+
+# # 1,2,3,4,5,6,7,8,9,10
+# # 3,3,4,5,6,7,8,9,10
+# # 6,4,5,6,7,8,9,10
+# # 10,5,6,7,8,9,10
+# # 15,6,7,8,9,10
+# # 21,7,8,9,10
+# # 28,8,9,10
+# # 36,9,10
+# # 45,10
+# # 55
+
+# print(reduce(add, l1, 0))
+
+# def multiply(a,b):
+#       return a*b
+
+# l1 = [1,2,3,4,5]
+
+# print(reduce(multiply, l1, 1))
+
+
+
+# recursive functions. 
+
+def sum(num):
+      if num == 1:
+            return 1
+      else:
+            return num + sum(num-1)
+
+# print(sum(1))
+
+# print(sum(2))
+
+print(dir(list))
+
+# l1 = [1,2,3,4,5,6,7,8,9,10]
+
+# for i in l1:
+#       print(sum(i))
+#       print("-" * 40)
+
+
+print(sum(100))
 """
 
 
